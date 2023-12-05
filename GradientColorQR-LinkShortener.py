844b5c9f4406730c0d66d36057b847552665061b
Praveen Kumar <pkrepswal2005@outlook.com>
2023-12-06 01:25:59 +0530

    The top and bottom color of qr code will not be of same color

--- GradientColorQR-LinkShortener.py
+++ GradientColorQR-LinkShortener.py
@@ -5,13 +5,17 @@ import qrcode
 from PIL import Image, ImageTk, ImageDraw, ImageOps
 import random
 
-colors = ['red','green','blue','orange','purple','pink','brown','cyan','magenta']
+colors = ['red','blue','orange','purple','pink','brown','cyan','magenta','gold','lime','chocolate','navy','violet','lavender','turquoise']
 def colr():
     return random.choice(colors)
 
 def create_gradient(size):
-    top = Image.new('RGB', size, color=colr())
-    bottom = Image.new('RGB', size, color=colr())
+    top_color = colr()
+    buttom_color = colr()
+    while top_color == buttom_color:
+        buttom_color = colr()
+    top = Image.new('RGB', size, color=top_color)
+    bottom = Image.new('RGB', size, color=buttom_color)
     mask = Image.new('L', size)
     mask_data = []
     for y in range(size[1]):
@@ -20,13 +24,12 @@ def create_gradient(size):
     return Image.composite(bottom, top, mask)
 
 
-# Function to shorten the URL
 def shorten_url():
     original_url = url_entry.get()
     if original_url:
         try:
             s = pyshorteners.Shortener()
-            shortened_url = s.tinyurl.short(original_url)  # You can use a different shortening service
+            shortened_url = s.tinyurl.short(original_url)
 
             # Display the shortened URL
             shortened_url_text.config(text=f"Shortened URL: {shortened_url}")
@@ -50,28 +53,26 @@ def shorten_url():
             mask = ImageOps.invert(mask)
             finl_img = Image.composite(gradient, qr_image, mask)
 
-            # Convert the QR code image to a PhotoImage for display in Tkinter
+            # Convert the QR code image into PhotoImage
             photo = ImageTk.PhotoImage(image=finl_img)
 
             # Display the QR code
             qr_image_label.config(image=photo)
-            qr_image_label.photo = photo  # Store a reference to avoid garbage collection
+            qr_image_label.photo = photo 
         except Exception as e:
             messagebox.showerror("Error", str(e))
     else:
         messagebox.showerror("Error", "Please enter a URL.")
 
-# Create a GUI window
 app = tk.Tk()
 app.title("URL Shortener and QR Code Generator")
 
-# Label and entry field for the original URL
+# Label to enter original URL
 url_label = tk.Label(app, text="Enter URL:")
 url_label.pack()
 url_entry = tk.Entry(app)
 url_entry.pack()
 
-# Button to shorten the URL
 shorten_button = tk.Button(app, text="Shorten URL", command=shorten_url)
 shorten_button.pack()
 
@@ -83,5 +84,4 @@ shortened_url_text.pack()
 qr_image_label = tk.Label(app)
 qr_image_label.pack()
 
-# Start the GUI main loop
 app.mainloop()
